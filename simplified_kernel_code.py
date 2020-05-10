44e3e9212789116be5c30963a07325403805b7c1
Matthew Kobilas <mattkobi789@gmail.com>
2020-05-09 20:53:09 -0400

    Added pack_gate for imitating the init of interrupt gates

--- simplified_kernel_code.py
+++ simplified_kernel_code.py
@@ -23,4 +23,11 @@ def __free_one_page(pfn:int, order:int):
 def __find_buddy_pfn(page_pfn:int, order:int):
     return page_pfn ^ (1 << order)
 
-__free_one_page(25, 0)
+def pack_gate(gate:int, type:int, base:int, dpl:int, flags:int, seg:int):
+    a = (seg << 16) | (base & 0xffff)
+    b = (base & 0xffff0000) | (((0x80 | type | (dpl << 5)) & 0xff) << 8)
+    print("a: ", format(a, "b"))
+    print("b: ", format(b, "b"))
+
+#__free_one_page(25, 0)
+pack_gate(0, 0xE, 0xCCCC0000, 0, 0, 96)
